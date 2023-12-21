20c7d4a525425e6129e572d864e084c7f9c8a7e5
Abin <abinmuhammed5@gmail.com>
2023-12-21 22:36:46 +0530

    node modules removed

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,23 @@
-require('dotenv').config()
-const mongoClient=require('mongodb').MongoClient
-const state={
-    db:null
-}
-const uri=
+require("dotenv").config();
+const mongoClient = require("mongodb").MongoClient;
+const state = {
+  db: null,
+};
+const uri = (module.exports.connect = function (done) {
+  const url = process.env.MONGODB_ATLAS;
+  // const url="mongodb://localhost:27017"
+  const dbname = "PLANTOGRAPHY";
 
-module.exports.connect=function (done){
-    const url=process.env.MONGODB_ATLAS
-    // const url="mongodb://localhost:27017"
-    const dbname="PLANTOGRAPHY"
-    
+  mongoClient.connect(url, (err, data) => {
+    if (err) {
+      console.log(err);
+      return done(err);
+    }
+    state.db = data.db(dbname);
+    done();
+  });
+});
 
-
- mongoClient.connect(url,(err,data)=>{
-        if (err) return done(err)
-        state.db=data.db(dbname)
-        done()
-    })
-
-}
-
-module.exports.get=function(){
-return state.db
-
-}
+module.exports.get = function () {
+  return state.db;
+};
